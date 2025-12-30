0bc98d0814129629ad067dd12c00f8b90c8ad5d7
Shaun Cheng <cheng754@gmail.com>
2025-12-30 11:12:37 -0800

    day 2

--- main.c
+++ main.c
@@ -5,18 +5,9 @@
 
 #include "mnist.h"
 
-static void print_error(const char *prefix, int err) { fprintf(stderr, "%s: %s\n", prefix, strerror(err)); }
-
-static void print_load_error(const char *kind, int err)
+static void print_error(const char *prefix, int err)
 {
-    if (err == EINVAL)
-    {
-        fprintf(stderr, "Failed to load %s: invalid MNIST IDX header or magic\n", kind);
-    }
-    else
-    {
-        print_error(kind, err);
-    }
+    fprintf(stderr, "%s: %s\n", prefix, strerror(err));
 }
 
 int main(int argc, char **argv)
@@ -29,28 +20,19 @@ int main(int argc, char **argv)
 
     MnistImages images = {0};
     MnistLabels labels = {0};
-    int exit_code = 0;
 
     int err = mnist_load_images(argv[1], &images);
     if (err)
     {
-        print_load_error("images", err);
+        print_error("Failed to load images", err);
         return 1;
     }
 
     err = mnist_load_labels(argv[2], &labels);
     if (err)
     {
-        print_load_error("labels", err);
+        print_error("Failed to load labels", err);
         mnist_free_images(&images);
-        return 1;
-    }
-    if (labels.count != images.count)
-    {
-        fprintf(stderr, "Label count (%u) does not match image count (%u)\n", labels.count,
-                images.count);
-        mnist_free_images(&images);
-        mnist_free_labels(&labels);
         return 1;
     }
 
@@ -58,18 +40,18 @@ int main(int argc, char **argv)
            labels.count);
 
     size_t ascii_size = 0;
-    int render_err = mnist_ascii_buffer_size(&images, &ascii_size);
-    if (!render_err)
+    err = mnist_ascii_buffer_size(&images, &ascii_size);
+    if (!err)
     {
         char *ascii = malloc(ascii_size);
         if (!ascii)
         {
-            render_err = ENOMEM;
+            err = ENOMEM;
         }
         else
         {
-            render_err = mnist_render_ascii(&images, 0, ascii, ascii_size);
-            if (!render_err)
+            err = mnist_render_ascii(&images, 0, ascii, ascii_size);
+            if (!err)
             {
                 fputs(ascii, stdout);
             }
@@ -77,13 +59,12 @@ int main(int argc, char **argv)
         }
     }
 
-    if (render_err)
+    if (err)
     {
-        print_error("Could not render sample", render_err);
-        exit_code = 1;
+        print_error("Could not render sample", err);
     }
 
     mnist_free_images(&images);
     mnist_free_labels(&labels);
-    return exit_code;
+    return 0;
 }
